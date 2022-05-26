9c972caf15a2b84c08c16b6d1d8df90cc46fda5f
j-fosstvedt <j.fosstvedt@gmail.com>
2022-05-26 17:52:48 +0200

    removed fish/fish folder

--- .config/fish/fish/functions/config.fish
+++
@@ -1,3 +0,0 @@
-function config --wraps='/usr/bin/git --git-dir=/home/jorgen/dotfiles/ --work-tree=/home/jorgen' --description 'alias config=/usr/bin/git --git-dir=/home/jorgen/dotfiles/ --work-tree=/home/jorgen'
-  /usr/bin/git --git-dir=/home/jorgen/dotfiles/ --work-tree=/home/jorgen $argv; 
-end
